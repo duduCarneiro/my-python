f1b99d91a51de1985b6b2b167d1130cfa83681cd
Eduardo Carneiro <dumaniaoctavio@hotmail.com>
2022-03-04 12:33:20 -0300

    done

--- first.py
+++ first.py
@@ -1,18 +1,31 @@
 import math
 import random
 import time
+
+def NthFibonacci(n):
+  fiveSquare = math.sqrt(5)
+  return (1/fiveSquare)*(((1 + fiveSquare)/2)**n - ((1 - fiveSquare)/2)**n)
+
+# applying formula - n from 0 to 40 - 1000 times
 start_time = time.time()
-fiveSquare = math.sqrt(5)
-
 for i in range(1001):
-  num = random.randrange(1,41)
-  print('what is the Fibonacci number of %d?'% num)
-  result = (1/fiveSquare)*(((1 + fiveSquare)/2)**num - ((1 - fiveSquare)/2)**num)
-  print (round(result))
-print("---it took %s seconds to be executed---" % (time.time() - start_time))
+  num = random.randrange(41)
+  print('using formula, what is the Fibonacci number of %d?'% num)
+  print (round(NthFibonacci(num)))
+firstLenghtTime = time.time() - start_time 
+  
+  
 
+fibonacciResults = []
 for i in range(41):
-  num = i
-  print('the Fibonacci number of %d'% num)
-  result = (1/fiveSquare)*(((1 + fiveSquare)/2)**num - ((1 - fiveSquare)/2)**num)
-  print (round(result))
+  fibonacciResults.append(round(NthFibonacci(i)))
+  
+# caching results - n from 0 to 40 - 1000 times  
+start_time = time.time()
+for i in range(1001):
+  num = random.randrange(41)
+  print('using cache, what is the Fibonacci number of %d?'% num)
+  print (fibonacciResults[num])
+secondLenghtTime = time.time() - start_time 
+print("---it took %f seconds to execute the formula---" % (firstLenghtTime))
+print("---it took %f seconds to execute with the cache---" % (secondLenghtTime))
