19a7d82ceb3380c129e334c2f440825d065c8299
Eduardo Carneiro <dumaniaoctavio@hotmail.com>
2022-03-29 17:23:56 -0300

    starting 100 days challenge

--- fibonacci.py
+++ fibonacci.py
@@ -4,22 +4,23 @@ def NthFibonacci(n):
   fiveSquare = math.sqrt(5)
   return round((1/fiveSquare)*(((1 + fiveSquare)/2)**n - ((1 - fiveSquare)/2)**n)) 
 
+fibonacciNumber = 1
+while fibonacciNumber:
+  try:
+    NthFibonacci(fibonacciNumber)
+  except:
+    print('out of range')
+    break
+  else:
+    print(NthFibonacci(fibonacciNumber))
+    
+  fibonacciNumber += 1
 
-loops = 101
-maxFibonacci = 1475
-
-for i in range(maxFibonacci + 1):
-  NthFibonacci(i)
-
-
-
+print(fibonacciNumber)
 
 
 # fibonacciResults = []
 # for i in range(maxFibonacci + 1):
 #   fibonacciResults.append(NthFibonacci(i))
-  
 # print(fibonacciResults)
 
-for i in range(maxFibonacci + 1):
-  NthFibonacci(i)
